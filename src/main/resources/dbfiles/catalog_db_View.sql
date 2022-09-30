a5d5f7e35918de1b03afaae60e3284df0e6f879f
Administrator <Administrator@demo.CATVM.local>
2022-09-30 12:03:08 +0100

    query

--- src/main/resources/dbfiles/catalog_db_View.sql
+++ src/main/resources/dbfiles/catalog_db_View.sql
@@ -135,3 +135,17 @@ from  Entity e INNER JOIN
 (select s.GUID as SchemaGuid,s.OmniformName as SchemaClass, t.TemplateID from SchemaClass s,Template t where t.TemplateGUID=s.GUID and  s.OmniformName in ('Product_Bundle_Property_Set','Product_Base_Property_Set')) as st
 on st.TemplateID = e.TemplateID 
 GO
+
+
+
+DROP VIEW  IF EXISTS CAPI_Bundle_Association
+GO
+create VIEW CAPI_Bundle_Association as
+select asoc.SchemaElement  ,  asoc.ElementValue as 'AssocationGUID', cb.ClassType 'ParentEntityClass' , cb.GUID as 'ParentEntityGuid',cb.Name as 'ParentEntityName',
+(select REPLACE(t.Name,' ','_') from InstanceElementValues inner join Entity e on ElementValue=e.guid  inner join Template t on e.TemplateID= t.TemplateID  where InstanceClassGUID= asoc.ElementValue and SchemaElementGUID = '5FB99F6C-7036-40DF-BF67-3666336FCF30') as 'ChildEntityClass',
+(select ElementValue from InstanceElementValues where InstanceClassGUID= asoc.ElementValue and SchemaElementGUID = '5FB99F6C-7036-40DF-BF67-3666336FCF30') as 'ChildEntityGUID',
+(select e.Name from InstanceElementValues inner join Entity e on ElementValue=e.guid where InstanceClassGUID= asoc.ElementValue and SchemaElementGUID = '5FB99F6C-7036-40DF-BF67-3666336FCF30') as 'ChildEntityName',
+Cast((select ElementValue from InstanceElementValues where InstanceClassGUID= asoc.ElementValue and SchemaElementGUID = 'A033E802-188F-4740-96B8-4536EFD93F46')as date) as 'AssociationStartDate',
+Cast((select ElementValue from InstanceElementValues where InstanceClassGUID= asoc.ElementValue and SchemaElementGUID = '5D7A88E8-A1D9-471D-884F-D6B9976C2315')as date) as 'AssociationEndDate'
+ from UV_InstanceElementValues asoc inner join CAPI_Bundle cb on asoc.InstanceClassGUID=cb.GUID where asoc.SchemaElement = 'Product Associations'
+ GO
